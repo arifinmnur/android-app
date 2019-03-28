45b7a6a7c3878ac103f461bbb425acdcacb25dd6
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-28 23:21:51 +0100

    Add some delay on the loading of a page in library

--- app/src/main/java/com/kelsos/mbrc/ui/navigation/library/LibraryPagerAdapter.kt
+++ app/src/main/java/com/kelsos/mbrc/ui/navigation/library/LibraryPagerAdapter.kt
@@ -3,12 +3,21 @@ package com.kelsos.mbrc.ui.navigation.library
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 class LibraryPagerAdapter(
   private val viewLifecycleOwner: LifecycleOwner
 ) : RecyclerView.Adapter<LibraryViewHolder>() {
   private var visiblePosition = 0
   private val screens: MutableList<LibraryScreen> = mutableListOf()
+  private val job: Job = Job()
+  private val scope: CoroutineScope = CoroutineScope(job + Dispatchers.Main)
+  private var deferred: Deferred<Unit>? = null
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryViewHolder {
     return LibraryViewHolder.create(parent)
@@ -18,12 +27,17 @@ class LibraryPagerAdapter(
     this.screens.clear()
     this.screens.addAll(screens)
   }
+
   override fun getItemCount(): Int = 4
 
   override fun onBindViewHolder(holder: LibraryViewHolder, position: Int) {
     val screen = screens[position]
     holder.bind(screen)
-    screen.observe(viewLifecycleOwner)
+    deferred?.cancel()
+    deferred = scope.async {
+      delay(400)
+      screen.observe(viewLifecycleOwner)
+    }
   }
 
   fun setVisiblePosition(itemPosition: Int) {
