e238ea1f00b055c7e6e9eb0174d4c05e1d601730
Konstantinos Paparas <kelsos86@gmail.com>
2019-04-20 22:14:54 +0200

    Work on Radio feature

--- app/src/main/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModel.kt
+++ app/src/main/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModel.kt
@@ -2,6 +2,7 @@ package com.kelsos.mbrc.features.radio.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.paging.PagedList
+import arrow.core.Try
 import com.kelsos.mbrc.di.modules.AppCoroutineDispatchers
 import com.kelsos.mbrc.features.radio.domain.RadioStation
 import com.kelsos.mbrc.features.radio.repository.RadioRepository
@@ -14,25 +15,38 @@ class RadioViewModel(
   private val radioRepository: RadioRepository,
   private val queue: QueueHandler,
   private val dispatchers: AppCoroutineDispatchers
-) : BaseViewModel<RadioRefreshResult>(dispatchers) {
+) : BaseViewModel<RadioUiMessages>(dispatchers) {
 
   val radios: LiveData<PagedList<RadioStation>> = radioRepository.getAll().paged()
 
   fun reload() {
     scope.launch(dispatchers.network) {
-      radioRepository.getRemote()
+      val result = radioRepository.getRemote()
         .toEither()
         .fold({
-          RadioRefreshResult.RefreshFailed
+          RadioUiMessages.RefreshFailed
         }, {
-          RadioRefreshResult.RefreshSuccess
+          RadioUiMessages.RefreshSuccess
         })
+      emit(result)
     }
   }
 
   fun play(path: String) {
     scope.launch(dispatchers.network) {
-      queue.queuePath(path)
+      val response = Try { queue.queuePath(path) }
+        .toEither()
+        .fold({
+          RadioUiMessages.NetworkError
+        }, { response ->
+          if (response.success) {
+            RadioUiMessages.QueueSuccess
+          } else {
+            RadioUiMessages.QueueFailed
+          }
+        })
+
+      emit(response)
     }
   }
 }
--- app/src/test/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModelTest.kt
+++ app/src/test/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModelTest.kt
@@ -4,14 +4,21 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import arrow.core.Try
 import com.google.common.truth.Truth.assertThat
 import com.kelsos.mbrc.TestApplication
+import com.kelsos.mbrc.events.Event
 import com.kelsos.mbrc.features.radio.repository.RadioRepository
 import com.kelsos.mbrc.helper.QueueHandler
+import com.kelsos.mbrc.helper.QueueResult
 import com.kelsos.mbrc.utils.MockFactory
 import com.kelsos.mbrc.utils.TestDispatchers
 import com.kelsos.mbrc.utils.observeOnce
+import io.mockk.CapturingSlot
+import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,32 +30,68 @@ import java.net.SocketTimeoutException
 class RadioViewModelTest {
 
   private lateinit var repository: RadioRepository
-  private lateinit var queueApi: QueueHandler
+  private lateinit var queueHandler: QueueHandler
   private lateinit var radioViewModel: RadioViewModel
+  private lateinit var observer: (Event<RadioUiMessages>) -> Unit
+  private lateinit var slot: CapturingSlot<Event<RadioUiMessages>>
 
   @Before
   fun setUp() {
     repository = mockk()
-    queueApi = mockk()
+    queueHandler = mockk()
+    observer = mockk()
+    slot = slot()
+    every { observer(capture(slot)) } just Runs
     every { repository.getAll() } answers { MockFactory(emptyList()) }
-    radioViewModel = RadioViewModel(repository, queueApi, TestDispatchers.dispatchers)
+    radioViewModel = RadioViewModel(repository, queueHandler, TestDispatchers.dispatchers)
   }
 
   @Test
   fun `should notify the observer that refresh failed`() {
     coEvery { repository.getRemote() } coAnswers { Try.raiseError(SocketTimeoutException()) }
+    radioViewModel.emitter.observeOnce(observer)
     radioViewModel.reload()
-    radioViewModel.emitter.observeOnce { event ->
-      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
-    }
+    verify(exactly = 1) { observer(any()) }
+    assertThat(slot.captured.peekContent()).isEqualTo(RadioUiMessages.RefreshFailed)
   }
 
   @Test
   fun `should notify the observer that refresh succeeded`() {
     coEvery { repository.getRemote() } coAnswers { Try.invoke { } }
+    radioViewModel.emitter.observeOnce(observer)
     radioViewModel.reload()
-    radioViewModel.emitter.observeOnce { event ->
-      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
+    verify(exactly = 1) { observer(any()) }
+    assertThat(slot.captured.peekContent()).isEqualTo(RadioUiMessages.RefreshSuccess)
+  }
+
+  @Test
+  fun `should call queue and notify success`() {
+    val playArguments = slot<String>()
+    coEvery { queueHandler.queuePath(capture(playArguments)) } answers {
+      QueueResult(true, 0)
     }
+
+    radioViewModel.emitter.observeOnce(observer)
+    radioViewModel.play("http://radio.station")
+    assertThat(playArguments.captured).contains("http://radio.station")
+    assertThat(slot.captured.peekContent()).isEqualTo(RadioUiMessages.QueueSuccess)
+  }
+
+  @Test
+  fun `should notify on network error`() {
+    coEvery { queueHandler.queuePath(any()) } throws SocketTimeoutException()
+    radioViewModel.emitter.observeOnce(observer)
+    radioViewModel.play("http://radio.station")
+    assertThat(slot.captured.peekContent()).isEqualTo(RadioUiMessages.NetworkError)
+  }
+
+  @Test
+  fun `should notify on queue failure`() {
+    coEvery { queueHandler.queuePath(any()) } answers {
+      QueueResult(false, 0)
+    }
+    radioViewModel.emitter.observeOnce(observer)
+    radioViewModel.play("http://radio.station")
+    assertThat(slot.captured.peekContent()).isEqualTo(RadioUiMessages.QueueFailed)
   }
 }
