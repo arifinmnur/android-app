JaylanT/mbrc

Remove PagedList/PagedList adapter since it breaks the whole experience especially when fast scrolling.

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/genres/BrowseGenreFragment.kt
@@ -63,7 +63,7 @@ class BrowseGenreFragment : Fragment(),
     presenter.detach()
   }
 
-  override fun update(pagedList: PagedList<GenreEntity>) {
+  override fun update(pagedList: List<GenreEntity>) {
     swipeLayout.isRefreshing = false
     adapter.setList(pagedList)
   }

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/genres/BrowseGenreFragment.kt
@@ -12,7 +12,6 @@ import androidx.annotation.IdRes
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -86,8 +85,8 @@ class BrowseGenreFragment : Fragment(),
     presenter.detach()
   }
 
-  override fun update(cursor: PagedList<GenreEntity>) {
-    adapter.submitList(cursor)
+  override fun update(cursor: List<GenreEntity>) {
+    adapter.setList(cursor)
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {