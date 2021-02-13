musicbeeremote/mbrc

Adds tests for RadioFragment

app/src/test/java/com/kelsos/mbrc/TestApplication.kt
@@ -22,6 +22,7 @@ class TestApplication : Application(), TestLifecycleApplication {
         System.out.println("$tag: $message")
       }
     })
+    setTheme(R.style.AppTheme)
   }
 
   override fun prepareTest(test: Any?) {

arifinmnur/android-app

app/src/test/java/com/kelsos/mbrc/TestApplication.kt
@@ -22,6 +22,7 @@ class TestApplication : Application(), TestLifecycleApplication {
         System.out.println("$tag: $message")
       }
     })
+    setTheme(R.style.Theme_App)
   }
 
   override fun prepareTest(test: Any?) {