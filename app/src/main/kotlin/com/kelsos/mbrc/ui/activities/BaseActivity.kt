musicbeeremote/mbrc

Removes Butterknife

app/src/main/kotlin/com/kelsos/mbrc/ui/activities/BaseActivity.kt
@@ -19,7 +19,6 @@ import android.view.KeyEvent
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import butterknife.BindView
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.events.ConnectionStatusChangeEvent
 import com.kelsos.mbrc.events.NotifyUser
@@ -41,6 +40,7 @@ import com.kelsos.mbrc.ui.navigation.nowplaying.NowPlayingActivity
 import com.kelsos.mbrc.ui.navigation.playlists.PlaylistActivity
 import com.kelsos.mbrc.ui.navigation.radio.RadioActivity
 import com.kelsos.mbrc.ui.preferences.SettingsActivity
+import kotterknife.bindView
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -50,9 +50,9 @@ abstract class BaseActivity : FontActivity(), NavigationView.OnNavigationItemSel
   @Inject lateinit var serviceChecker: ServiceChecker
   @Inject lateinit var volumeInteractor: VolumeInteractor
 
-  @BindView(R.id.toolbar) lateinit var toolbar: Toolbar
-  @BindView(R.id.drawer_layout) lateinit var drawer: DrawerLayout
-  @BindView(R.id.nav_view) lateinit var navigationView: NavigationView
+  private val toolbar: Toolbar by bindView(R.id.toolbar)
+  private val drawer: DrawerLayout by bindView(R.id.drawer_layout)
+  private val navigationView: NavigationView by bindView(R.id.nav_view)
 
   private var connectText: TextView? = null
   private var toggle: ActionBarDrawerToggle? = null
@@ -211,7 +211,7 @@ abstract class BaseActivity : FontActivity(), NavigationView.OnNavigationItemSel
   }
 
   /**
-   * Should be called after injections and Butterknife bindings.
+   * Should be called after injections and
    */
   fun setup() {
     Timber.v("Initializing base activity")

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/LibraryActivity.kt
@@ -13,11 +13,10 @@ import android.support.v7.widget.SearchView.OnQueryTextListener
 import android.text.TextUtils
 import android.view.Menu
 import android.view.MenuItem
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.ui.activities.BaseActivity
 import com.kelsos.mbrc.ui.navigation.library.search.SearchResultsActivity
+import kotterknife.bindView
 import toothpick.Scope
 import toothpick.Toothpick
 import toothpick.smoothie.module.SmoothieActivityModule
@@ -28,8 +27,8 @@ class LibraryActivity : BaseActivity(),
     OnQueryTextListener,
     OnPageChangeListener {
 
-  @BindView(R.id.search_pager) lateinit var pager: ViewPager
-  @BindView(R.id.pager_tab_strip) lateinit var tabs: TabLayout
+  private val pager: ViewPager by bindView(R.id.search_pager)
+  private val tabs: TabLayout by bindView(R.id.pager_tab_strip)
 
   private var searchView: SearchView? = null
   private var searchMenuItem: MenuItem? = null
@@ -77,7 +76,7 @@ class LibraryActivity : BaseActivity(),
     super.onCreate(savedInstanceState)
     Toothpick.inject(this, scope)
     setContentView(R.layout.activity_library)
-    ButterKnife.bind(this)
+
     super.setup()
     pagerAdapter = LibraryPagerAdapter(this)
     pager.adapter = pagerAdapter

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/artist_albums/ArtistAlbumsActivity.kt
@@ -5,8 +5,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import android.widget.LinearLayout
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.content.library.albums.Album
 import com.kelsos.mbrc.ui.activities.FontActivity
@@ -14,6 +12,7 @@ import com.kelsos.mbrc.ui.navigation.library.PopupActionHandler
 import com.kelsos.mbrc.ui.navigation.library.albums.AlbumEntryAdapter
 import com.kelsos.mbrc.ui.widgets.EmptyRecyclerView
 import com.raizlabs.android.dbflow.list.FlowCursorList
+import kotterknife.bindView
 import toothpick.Scope
 import toothpick.Toothpick
 import toothpick.smoothie.module.SmoothieActivityModule
@@ -23,9 +22,9 @@ class ArtistAlbumsActivity : FontActivity(),
     ArtistAlbumsView,
     AlbumEntryAdapter.MenuItemSelectedListener {
 
-  @BindView(R.id.album_recycler) lateinit var recyclerView: EmptyRecyclerView
-  @BindView(R.id.toolbar) lateinit var toolbar: Toolbar
-  @BindView(R.id.empty_view) lateinit var emptyView: LinearLayout
+  private val recyclerView: EmptyRecyclerView by bindView(R.id.album_recycler)
+  private val toolbar: Toolbar by bindView(R.id.toolbar)
+  private val emptyView: LinearLayout by bindView(R.id.empty_view)
 
   @Inject lateinit var actionHandler: PopupActionHandler
   @Inject lateinit var adapter: AlbumEntryAdapter
@@ -41,7 +40,7 @@ class ArtistAlbumsActivity : FontActivity(),
     super.onCreate(savedInstanceState)
     Toothpick.inject(this, scope)
     setContentView(R.layout.activity_artist_albums)
-    ButterKnife.bind(this)
+
 
     val extras = intent.extras
     if (extras != null) {

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/ui/activities/BaseActivity.kt
@@ -16,7 +16,6 @@ import androidx.core.app.TaskStackBuilder
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import butterknife.BindView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -41,6 +40,7 @@ import com.kelsos.mbrc.ui.navigation.nowplaying.NowPlayingActivity
 import com.kelsos.mbrc.ui.navigation.playlists.PlaylistActivity
 import com.kelsos.mbrc.ui.navigation.radio.RadioActivity
 import com.kelsos.mbrc.ui.preferences.SettingsActivity
+import kotterknife.bindView
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -51,16 +51,12 @@ abstract class BaseActivity : FontActivity(), NavigationView.OnNavigationItemSel
   @Inject
   lateinit var serviceChecker: ServiceChecker
 
-  @Inject lateinit var volumeInteractor: VolumeInteractor
+  @Inject
+  lateinit var volumeInteractor: VolumeInteractor
 
-  @BindView(R.id.toolbar)
-  lateinit var toolbar: MaterialToolbar
-
-  @BindView(R.id.drawer_layout)
-  lateinit var drawer: DrawerLayout
-
-  @BindView(R.id.nav_view)
-  lateinit var navigationView: NavigationView
+  private val toolbar: MaterialToolbar by bindView(R.id.toolbar)
+  private val drawer: DrawerLayout by bindView(R.id.drawer_layout)
+  private val navigationView: NavigationView by bindView(R.id.nav_view)
 
   private var connectText: TextView? = null
   private var toggle: ActionBarDrawerToggle? = null
@@ -223,7 +219,7 @@ abstract class BaseActivity : FontActivity(), NavigationView.OnNavigationItemSel
   }
 
   /**
-   * Should be called after injections and Butterknife bindings.
+   * Should be called after injections and
    */
   fun setup() {
     Timber.v("Initializing base activity")

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/LibraryActivity.kt
@@ -8,8 +8,6 @@ import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.isGone
 import androidx.viewpager2.widget.ViewPager2
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.progressindicator.ProgressIndicator
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +15,7 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.ui.activities.BaseActivity
+import kotterknife.bindView
 import toothpick.Scope
 import toothpick.Toothpick
 import toothpick.smoothie.module.SmoothieActivityModule
@@ -26,11 +25,8 @@ class LibraryActivity : BaseActivity(),
   LibraryView,
   OnQueryTextListener {
 
-  @BindView(R.id.search_pager)
-  lateinit var pager: ViewPager2
-
-  @BindView(R.id.pager_tab_strip)
-  lateinit var tabs: TabLayout
+  private val pager: ViewPager2 by bindView(R.id.search_pager)
+  private val tabs: TabLayout by bindView(R.id.pager_tab_strip)
 
   private var searchView: SearchView? = null
   private var searchMenuItem: MenuItem? = null
@@ -81,7 +77,7 @@ class LibraryActivity : BaseActivity(),
     super.onCreate(savedInstanceState)
     Toothpick.inject(this, scope)
     setContentView(R.layout.activity_library)
-    ButterKnife.bind(this)
+
     super.setup()
     pagerAdapter = LibraryPagerAdapter(this)
     pager.apply {

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/artist_albums/ArtistAlbumsActivity.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.snackbar.Snackbar
 import com.kelsos.mbrc.R
-import com.kelsos.mbrc.content.now_playing.queue.Queue
 import com.kelsos.mbrc.content.library.albums.Album
+import com.kelsos.mbrc.content.now_playing.queue.Queue
 import com.kelsos.mbrc.ui.activities.FontActivity
 import com.kelsos.mbrc.ui.navigation.library.PopupActionHandler
 import com.kelsos.mbrc.ui.navigation.library.albums.AlbumEntryAdapter
 import com.kelsos.mbrc.ui.widgets.EmptyRecyclerView
 import com.raizlabs.android.dbflow.list.FlowCursorList
+import kotterknife.bindView
 import toothpick.Scope
 import toothpick.Toothpick
 import toothpick.smoothie.module.SmoothieActivityModule
@@ -25,9 +24,9 @@ class ArtistAlbumsActivity : FontActivity(),
   ArtistAlbumsView,
   AlbumEntryAdapter.MenuItemSelectedListener {
 
-  @BindView(R.id.album_recycler) lateinit var recyclerView: EmptyRecyclerView
-  @BindView(R.id.toolbar) lateinit var toolbar: MaterialToolbar
-  @BindView(R.id.empty_view) lateinit var emptyView: ConstraintLayout
+  private val recyclerView: EmptyRecyclerView by bindView(R.id.album_recycler)
+  private val toolbar: MaterialToolbar by bindView(R.id.toolbar)
+  private val emptyView: ConstraintLayout by bindView(R.id.empty_view)
 
   @Inject lateinit var actionHandler: PopupActionHandler
   @Inject lateinit var adapter: AlbumEntryAdapter
@@ -45,7 +44,7 @@ class ArtistAlbumsActivity : FontActivity(),
     super.onCreate(savedInstanceState)
     Toothpick.inject(this, scope)
     setContentView(R.layout.activity_artist_albums)
-    ButterKnife.bind(this)
+
 
     val extras = intent.extras
     if (extras != null) {