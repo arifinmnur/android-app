JaylanT/mbrc

Trying to optimize the library functionality

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/responses/Position.kt
@@ -3,7 +3,6 @@ package com.kelsos.mbrc.networking.protocol.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Position(
-    @JsonProperty("current") val current: Int,
-    @JsonProperty("total") val total: Int
+  @JsonProperty("current") val current: Int,
+  @JsonProperty("total") val total: Int
 )
-

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/responses/Position.kt
@@ -3,7 +3,6 @@ package com.kelsos.mbrc.networking.protocol.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Position(
-    @JsonProperty("current") val current: Long,
-    @JsonProperty("total") val total: Long
+  @JsonProperty("current") val current: Long,
+  @JsonProperty("total") val total: Long
 )
-