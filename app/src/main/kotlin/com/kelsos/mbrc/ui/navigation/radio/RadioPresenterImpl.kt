musicbeeremote/mbrc

Changes view to an accessor in BasePresenter. The accessor will throw if the view is not attached (null)

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/radio/RadioPresenterImpl.kt
@@ -21,7 +21,8 @@ class RadioPresenterImpl
     RadioPresenter {
 
   override fun load() {
-    view?.showLoading()
+
+    view().showLoading()
     addDisposable(radioRepository.cacheIsEmpty().flatMap {
       if (it) {
         return@flatMap radioRepository.getAndSaveRemote()
@@ -29,26 +30,26 @@ class RadioPresenterImpl
         return@flatMap radioRepository.getAllCursor()
       }
     }.subscribeOn(ioScheduler).observeOn(mainScheduler).subscribe({
-      view?.hideLoading()
-      view?.update(it)
+      view().hideLoading()
+      view().update(it)
     }, {
-      view?.error(it)
-      view?.hideLoading()
+      view().error(it)
+      view().hideLoading()
       Timber.v(it, "Failed")
     }))
   }
 
   override fun refresh() {
-    view?.showLoading()
+    view().showLoading()
     addDisposable(radioRepository.getAndSaveRemote()
         .subscribeOn(ioScheduler)
         .observeOn(mainScheduler)
         .subscribe({
-          view?.update(it)
-          view?.hideLoading()
+          view().update(it)
+          view().hideLoading()
         }, {
-          view?.error(it)
-          view?.hideLoading()
+          view().error(it)
+          view().hideLoading()
         }))
   }
 
@@ -57,9 +58,9 @@ class RadioPresenterImpl
         .subscribeOn(ioScheduler)
         .observeOn(mainScheduler)
         .subscribe({
-          view?.radioPlaySuccessful()
+          view().radioPlaySuccessful()
         }, {
-          view?.radioPlayFailed(it)
+          view().radioPlayFailed(it)
         })
   }
 }

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/radio/RadioPresenterImpl.kt
@@ -15,26 +15,27 @@ constructor(
 ) : BasePresenter<RadioView>(), RadioPresenter {
 
   override fun load() {
-    view?.showLoading()
+
+    view().showLoading()
     scope.launch {
       try {
-        view?.update(radioRepository.getAndSaveRemote())
+        view().update(radioRepository.getAndSaveRemote())
       } catch (e: Exception) {
-        view?.error(e)
+        view().error(e)
       }
-      view?.hideLoading()
+      view().hideLoading()
     }
   }
 
   override fun refresh() {
-    view?.showLoading()
+    view().showLoading()
     scope.launch {
       try {
-        view?.update(radioRepository.getAndSaveRemote())
+        view().update(radioRepository.getAndSaveRemote())
       } catch (e: Exception) {
-        view?.error(e)
+        view().error(e)
       }
-      view?.hideLoading()
+      view().hideLoading()
     }
   }
 
@@ -42,9 +43,9 @@ constructor(
     scope.launch {
       try {
         queue.queuePath(path)
-        view?.radioPlaySuccessful()
+        view().radioPlaySuccessful()
       } catch (e: Exception) {
-        view?.radioPlayFailed(e)
+        view().radioPlayFailed(e)
       }
     }
   }