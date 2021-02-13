JaylanT/mbrc

WIP: Sync and library support

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/tracks/BrowseTrackPresenterImpl.kt
@@ -8,6 +8,7 @@ import com.kelsos.mbrc.events.LibraryRefreshCompleteEvent
 import com.kelsos.mbrc.events.bus.RxBus
 import com.kelsos.mbrc.mvp.BasePresenter
 import com.kelsos.mbrc.utilities.SchedulerProvider
+import io.reactivex.rxkotlin.plusAssign
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -24,7 +25,10 @@ constructor(
 
   override fun attach(view: BrowseTrackView) {
     super.attach(view)
-    bus.register(this, LibraryRefreshCompleteEvent::class.java, { load() })
+    disposables += bus.observe(LibraryRefreshCompleteEvent::class)
+        .observeOn(schedulerProvider.main())
+        .subscribeOn(schedulerProvider.io())
+        .subscribe { load() }
   }
 
   override fun detach() {
@@ -33,46 +37,46 @@ constructor(
   }
 
   override fun load() {
-    view().showLoading()
-    addDisposable(repository.getAll()
+    disposables += repository.getAll()
         .observeOn(schedulerProvider.main())
         .subscribeOn(schedulerProvider.io())
+        .doFinally {
+          view().hideLoading()
+        }
         .subscribe({
           onTrackLoad(it)
-          view().hideLoading()
         }, {
-          Timber.v(it, "Error while loading the data from the database")
           view().failure(it)
-          view().hideLoading()
-        }))
+          Timber.e(it, "Error while loading the data from the database")
+        })
   }
 
-  private fun onTrackLoad(it: LiveData<List<TrackEntity>>) {
+  override fun reload() {
+    disposables += repository.getAndSaveRemote()
+        .observeOn(schedulerProvider.main())
+        .subscribeOn(schedulerProvider.io())
+        .doFinally {
+          view().hideLoading()
+        }
+        .subscribe({
+          onTrackLoad(it)
+        }, {
+          view().failure(it)
+          Timber.e(it, "Error while loading the data from the database")
+        })
+  }
+
+  private fun onTrackLoad(data: LiveData<List<TrackEntity>>) {
     if (::tracks.isInitialized) {
       tracks.removeObservers(this)
     }
 
-    tracks = it
+    tracks = data
     tracks.observe(this, Observer {
       if (it != null) {
         view().update(it)
       }
     })
-  }
-
-  override fun reload() {
-    view().showLoading()
-    addDisposable(repository.getAndSaveRemote()
-        .observeOn(schedulerProvider.main())
-        .subscribeOn(schedulerProvider.io())
-        .subscribe({
-          onTrackLoad(it)
-          view().hideLoading()
-        }, {
-          Timber.v(it, "Error while loading the data from the database")
-          view().failure(it)
-          view().hideLoading()
-        }))
   }
 
 }

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/tracks/BrowseTrackPresenterImpl.kt
@@ -9,6 +9,8 @@ import com.kelsos.mbrc.events.bus.RxBus
 import com.kelsos.mbrc.helper.QueueHandler
 import com.kelsos.mbrc.mvp.BasePresenter
 import com.kelsos.mbrc.ui.navigation.library.LibrarySearchModel
+import com.kelsos.mbrc.utilities.SchedulerProvider
+import io.reactivex.rxkotlin.plusAssign
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -20,7 +22,8 @@ constructor(
   private val repository: TrackRepository,
   private val librarySyncInteractor: LibrarySyncInteractor,
   private val queue: QueueHandler,
-  private val searchModel: LibrarySearchModel
+  private val searchModel: LibrarySearchModel,
+  private val schedulerProvider: SchedulerProvider
 ) : BasePresenter<BrowseTrackView>(),
   BrowseTrackPresenter {
 
@@ -32,7 +35,10 @@ constructor(
 
   override fun attach(view: BrowseTrackView) {
     super.attach(view)
-    bus.register(this, LibraryRefreshCompleteEvent::class.java) { load() }
+    disposables += bus.observe(LibraryRefreshCompleteEvent::class)
+      .observeOn(schedulerProvider.main())
+      .subscribeOn(schedulerProvider.io())
+      .subscribe { load() }
   }
 
   override fun detach() {
@@ -46,7 +52,6 @@ constructor(
 
   private fun updateUi(term: String) {
     scope.launch {
-      view().showLoading()
       view().search(term)
       try {
         onTrackLoad(getData(term))
@@ -56,20 +61,6 @@ constructor(
       view().hideLoading()
     }
   }
-
-  private fun onTrackLoad(it: LiveData<List<TrackEntity>>) {
-    if (::tracks.isInitialized) {
-      tracks.removeObservers(this)
-    }
-
-    tracks = it
-    tracks.observe(this@BrowseTrackPresenterImpl, {
-      if (it != null) {
-        view().update(it)
-      }
-    })
-  }
-
 
   private suspend fun getData(term: String): LiveData<List<TrackEntity>> {
     return if (term.isEmpty()) {
@@ -94,5 +85,18 @@ constructor(
       }
       view().queue(success, tracks)
     }
+  }
+
+  private fun onTrackLoad(data: LiveData<List<TrackEntity>>) {
+    if (::tracks.isInitialized) {
+      tracks.removeObservers(this)
+    }
+
+    tracks = data
+    tracks.observe(this@BrowseTrackPresenterImpl, {
+      if (it != null) {
+        view().update(it)
+      }
+    })
   }
 }