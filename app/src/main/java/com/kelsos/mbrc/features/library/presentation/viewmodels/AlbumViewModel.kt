musicbeeremote/mbrc

Working on library search functionality

app/src/main/java/com/kelsos/mbrc/features/library/presentation/viewmodels/AlbumViewModel.kt
@@ -1,12 +1,36 @@
 package com.kelsos.mbrc.features.library.presentation.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MediatorLiveData
 import androidx.paging.PagedList
+import com.kelsos.mbrc.common.utilities.AppCoroutineDispatchers
 import com.kelsos.mbrc.common.utilities.paged
 import com.kelsos.mbrc.features.library.data.Album
+import com.kelsos.mbrc.features.library.presentation.LibrarySearchModel
 import com.kelsos.mbrc.features.library.repositories.AlbumRepository
+import com.kelsos.mbrc.ui.BaseViewModel
+import com.kelsos.mbrc.ui.UiMessageBase
+import kotlinx.coroutines.flow.onEach
 
-class AlbumViewModel(repository: AlbumRepository) : ViewModel() {
-  val albums: LiveData<PagedList<Album>> = repository.getAlbumsSorted().paged()
+class AlbumViewModel(
+  private val repository: AlbumRepository,
+  searchModel: LibrarySearchModel,
+  dispatchers: AppCoroutineDispatchers
+) : BaseViewModel<UiMessageBase>(dispatchers) {
+  private val _albums: MediatorLiveData<PagedList<Album>> = MediatorLiveData()
+  val albums: LiveData<PagedList<Album>>
+    get() = _albums
+
+  init {
+    var lastSource = repository.getAlbumsSorted().paged()
+    _albums.addSource(lastSource) { data -> _albums.value = data }
+
+    searchModel.search.onEach {
+      _albums.removeSource(lastSource)
+
+      val factory = if (it.isEmpty()) repository.getAll() else repository.search(it)
+      lastSource = factory.paged()
+      _albums.addSource(lastSource) { data -> _albums.value = data }
+    }
+  }
 }

app/src/main/java/com/kelsos/mbrc/features/library/presentation/viewmodels/ArtistViewModel.kt
@@ -1,12 +1,36 @@
 package com.kelsos.mbrc.features.library.presentation.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MediatorLiveData
 import androidx.paging.PagedList
+import com.kelsos.mbrc.common.utilities.AppCoroutineDispatchers
 import com.kelsos.mbrc.common.utilities.paged
 import com.kelsos.mbrc.features.library.data.Artist
+import com.kelsos.mbrc.features.library.presentation.LibrarySearchModel
 import com.kelsos.mbrc.features.library.repositories.ArtistRepository
+import com.kelsos.mbrc.ui.BaseViewModel
+import com.kelsos.mbrc.ui.UiMessageBase
+import kotlinx.coroutines.flow.onEach
 
-class ArtistViewModel(repository: ArtistRepository) : ViewModel() {
-  val artists: LiveData<PagedList<Artist>> = repository.allArtists().paged()
+class ArtistViewModel(
+  private val repository: ArtistRepository,
+  searchModel: LibrarySearchModel,
+  dispatchers: AppCoroutineDispatchers
+) : BaseViewModel<UiMessageBase>(dispatchers) {
+  private val _artists: MediatorLiveData<PagedList<Artist>> = MediatorLiveData()
+  val artists: LiveData<PagedList<Artist>>
+    get() = _artists
+
+  init {
+    var lastSource = repository.allArtists().paged()
+    _artists.addSource(lastSource) { data -> _artists.value = data }
+
+    searchModel.search.onEach {
+      _artists.removeSource(lastSource)
+
+      val factory = if (it.isEmpty()) repository.getAll() else repository.search(it)
+      lastSource = factory.paged()
+      _artists.addSource(lastSource) { data -> _artists.value = data }
+    }
+  }
 }

arifinmnur/android-app

app/src/main/java/com/kelsos/mbrc/features/library/presentation/viewmodels/AlbumViewModel.kt
@@ -1,12 +1,36 @@
 package com.kelsos.mbrc.features.library.presentation.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MediatorLiveData
 import androidx.paging.PagedList
+import com.kelsos.mbrc.common.utilities.AppCoroutineDispatchers
 import com.kelsos.mbrc.common.utilities.paged
 import com.kelsos.mbrc.features.library.data.Album
+import com.kelsos.mbrc.features.library.presentation.LibrarySearchModel
 import com.kelsos.mbrc.features.library.repositories.AlbumRepository
+import com.kelsos.mbrc.ui.BaseViewModel
+import com.kelsos.mbrc.ui.UiMessageBase
+import kotlinx.coroutines.flow.onEach
 
-class AlbumViewModel(repository: AlbumRepository) : ViewModel() {
-  val albums: LiveData<PagedList<Album>> = repository.getAll().paged()
+class AlbumViewModel(
+  private val repository: AlbumRepository,
+  searchModel: LibrarySearchModel,
+  dispatchers: AppCoroutineDispatchers
+) : BaseViewModel<UiMessageBase>(dispatchers) {
+  private val _albums: MediatorLiveData<PagedList<Album>> = MediatorLiveData()
+  val albums: LiveData<PagedList<Album>>
+    get() = _albums
+
+  init {
+    var lastSource = repository.getAll().paged()
+    _albums.addSource(lastSource) { data -> _albums.value = data }
+
+    searchModel.search.onEach {
+      _albums.removeSource(lastSource)
+
+      val factory = if (it.isEmpty()) repository.getAll() else repository.search(it)
+      lastSource = factory.paged()
+      _albums.addSource(lastSource) { data -> _albums.value = data }
+    }
+  }
 }

app/src/main/java/com/kelsos/mbrc/features/library/presentation/viewmodels/ArtistViewModel.kt
@@ -1,12 +1,36 @@
 package com.kelsos.mbrc.features.library.presentation.viewmodels
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MediatorLiveData
 import androidx.paging.PagedList
+import com.kelsos.mbrc.common.utilities.AppCoroutineDispatchers
 import com.kelsos.mbrc.common.utilities.paged
 import com.kelsos.mbrc.features.library.data.Artist
+import com.kelsos.mbrc.features.library.presentation.LibrarySearchModel
 import com.kelsos.mbrc.features.library.repositories.ArtistRepository
+import com.kelsos.mbrc.ui.BaseViewModel
+import com.kelsos.mbrc.ui.UiMessageBase
+import kotlinx.coroutines.flow.onEach
 
-class ArtistViewModel(repository: ArtistRepository) : ViewModel() {
-  val artists: LiveData<PagedList<Artist>> = repository.getAll().paged()
+class ArtistViewModel(
+  private val repository: ArtistRepository,
+  searchModel: LibrarySearchModel,
+  dispatchers: AppCoroutineDispatchers
+) : BaseViewModel<UiMessageBase>(dispatchers) {
+  private val _artists: MediatorLiveData<PagedList<Artist>> = MediatorLiveData()
+  val artists: LiveData<PagedList<Artist>>
+    get() = _artists
+
+  init {
+    var lastSource = repository.getAll().paged()
+    _artists.addSource(lastSource) { data -> _artists.value = data }
+
+    searchModel.search.onEach {
+      _artists.removeSource(lastSource)
+
+      val factory = if (it.isEmpty()) repository.getAll() else repository.search(it)
+      lastSource = factory.paged()
+      _artists.addSource(lastSource) { data -> _artists.value = data }
+    }
+  }
 }