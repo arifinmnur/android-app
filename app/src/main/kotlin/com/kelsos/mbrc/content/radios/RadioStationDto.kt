JaylanT/mbrc

refactor: Replacing jackson with moshi
Needed to get rid of kotlin-reflect and since moshi 1.6 you can have
generated adapters for kotlin data classes.

app/src/main/kotlin/com/kelsos/mbrc/content/radios/RadioStationDto.kt
@@ -1,10 +1,12 @@
 package com.kelsos.mbrc.content.radios
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class RadioStationDto(
-  @JsonProperty("name")
+  @Json(name="name")
   var name: String = "",
-  @JsonProperty("url")
+  @Json(name="url")
   var url: String = ""
 )

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/Page.kt
@@ -1,21 +1,17 @@
 package com.kelsos.mbrc.networking.protocol
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.util.ArrayList
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-
-@JsonPropertyOrder("total", "offset", "limit", "data")
+@JsonClass(generateAdapter = true)
 class Page<T> {
-
-  @JsonProperty("total")
+  @Json(name="total")
   var total: Int = 0
-  @JsonProperty("offset")
+  @Json(name="offset")
   var offset: Int = 0
-  @JsonProperty("limit")
+  @Json(name="limit")
   var limit: Int = 0
-  @JsonProperty("data")
+  @Json(name="data")
   var data: List<T> = ArrayList()
 }

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/content/radios/RadioStationDto.kt
@@ -1,10 +1,12 @@
 package com.kelsos.mbrc.content.radios
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class RadioStationDto(
-  @JsonProperty("name")
+  @Json(name = "name")
   var name: String = "",
-  @JsonProperty("url")
+  @Json(name = "url")
   var url: String = ""
 )

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/Page.kt
@@ -1,24 +1,17 @@
 package com.kelsos.mbrc.networking.protocol
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.util.ArrayList
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-
-@JsonPropertyOrder("total", "offset", "limit", "data")
+@JsonClass(generateAdapter = true)
 class Page<T> {
-
-  @JsonProperty("total")
+  @Json(name = "total")
   var total: Int = 0
-
-  @JsonProperty("offset")
+  @Json(name = "offset")
   var offset: Int = 0
-
-  @JsonProperty("limit")
+  @Json(name = "limit")
   var limit: Int = 0
-
-  @JsonProperty("data")
+  @Json(name = "data")
   var data: List<T> = ArrayList()
 }