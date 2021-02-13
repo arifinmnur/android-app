musicbeeremote/mbrc

Work on #143 updating to more PagedListAdapters

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/genreartists/GenreArtistsActivity.kt
@@ -1,5 +1,6 @@
 package com.kelsos.mbrc.ui.navigation.library.genreartists
 
+import android.arch.paging.PagedList
 import android.os.Bundle
 import android.support.constraint.Group
 import android.support.v7.widget.RecyclerView
@@ -65,16 +66,16 @@ class GenreArtistsActivity : BaseActivity(),
     return super.onOptionsItemSelected(item)
   }
 
-  override fun onMenuItemSelected(menuItem: MenuItem, entry: ArtistEntity) {
-    actionHandler.artistSelected(menuItem, entry, this)
+  override fun onMenuItemSelected(action: String, entry: ArtistEntity) {
+    actionHandler.artistSelected(action, entry, this)
   }
 
   override fun onItemClicked(artist: ArtistEntity) {
     actionHandler.artistSelected(artist, this)
   }
 
-  override fun update(data: List<ArtistEntity>) {
-   adapter.update(data)
+  override fun update(pagedList: PagedList<ArtistEntity>) {
+    adapter.setList(pagedList)
   }
 
   override fun onDestroy() {

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/tracks/TrackEntryAdapter.kt
@@ -1,19 +1,19 @@
 package com.kelsos.mbrc.ui.navigation.library.tracks
 
-
 import android.arch.paging.PagedListAdapter
 import android.support.v7.recyclerview.extensions.DiffCallback
-import android.support.v7.widget.PopupMenu
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.content.library.tracks.TrackEntity
+import com.kelsos.mbrc.content.nowplaying.queue.LibraryPopup
+import com.kelsos.mbrc.content.nowplaying.queue.LibraryPopup.Action
 import com.kelsos.mbrc.extensions.string
+import com.kelsos.mbrc.ui.navigation.library.popup
 import com.kelsos.mbrc.ui.widgets.RecyclerViewFastScroller.BubbleTextGetter
 import com.kelsos.mbrc.utilities.Checks.ifNotNull
 import kotterknife.bindView
@@ -22,47 +22,46 @@ import javax.inject.Inject
 class TrackEntryAdapter
 @Inject
 constructor() : PagedListAdapter<TrackEntity, TrackEntryAdapter.ViewHolder>(DIFF_CALLBACK), BubbleTextGetter {
-  
+
   private var listener: MenuItemSelectedListener? = null
+  private val indicatorPressed: (View, Int) -> Unit = { view, position ->
+    view.popup(R.menu.popup_track) {
+
+      val action = when (it) {
+        R.id.popup_track_queue_next -> LibraryPopup.NEXT
+        R.id.popup_track_queue_last -> LibraryPopup.LAST
+        R.id.popup_track_play -> LibraryPopup.NOW
+        R.id.popup_track_play_queue_all -> LibraryPopup.ADD_ALL
+        else -> throw IllegalArgumentException("invalid menuItem id $it")
+      }
+
+      ifNotNull(listener, getItem(position)) { listener, track ->
+        listener.onMenuItemSelected(action, track)
+      }
+    }
+  }
+
+  private val pressed: (View, Int) -> Unit = { _, position ->
+    ifNotNull(listener, getItem(position)) { listener, track ->
+      listener.onItemClicked(track)
+    }
+  }
 
   fun setMenuItemSelectedListener(listener: MenuItemSelectedListener) {
     this.listener = listener
   }
 
-  
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val holder = ViewHolder.create(parent)
-    
-    holder.indicator.setOnClickListener { createPopup(it, holder) }
-
-    holder.itemView.setOnClickListener {
-      val position = holder.adapterPosition
-      ifNotNull(listener, getItem(position)) { listener, track ->
-        listener.onItemClicked(track)
-      }
-
-    }
-    return holder
-  }
-
-  private fun createPopup(it: View, holder: ViewHolder) {
-    val popupMenu = PopupMenu(it.context, it)
-    popupMenu.inflate(R.menu.popup_track)
-    popupMenu.setOnMenuItemClickListener { menuItem ->
-      val position = holder.adapterPosition
-      ifNotNull(listener, getItem(position)) { listener, track ->
-        listener.onMenuItemSelected(menuItem, track)
-      }
-      true
-    }
-    popupMenu.show()
+    return ViewHolder.create(parent, indicatorPressed, pressed)
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    val entry = getItem(holder.adapterPosition)
-    entry?.let { (artist, title) ->
-      holder.title.text = title
-      holder.artist.text = if (artist.isBlank()) holder.unknownArtist else artist
+    val trackEntity = getItem(holder.adapterPosition)
+
+    if (trackEntity != null) {
+      holder.bindTo(trackEntity)
+    } else {
+      holder.clear()
     }
   }
 
@@ -87,23 +86,46 @@ constructor() : PagedListAdapter<TrackEntity, TrackEntryAdapter.ViewHolder>(DIFF
   }
 
   interface MenuItemSelectedListener {
-    fun onMenuItemSelected(menuItem: MenuItem, entry: TrackEntity)
+    fun onMenuItemSelected(@Action action: String, entry: TrackEntity)
 
     fun onItemClicked(track: TrackEntity)
   }
 
-  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val artist: TextView by bindView(R.id.line_two)
-    val title: TextView by bindView(R.id.line_one)
-    val indicator: ImageView by bindView(R.id.overflow_menu)
-    val unknownArtist: String by lazy { string(R.string.unknown_artist) }
-    
+  class ViewHolder(
+      itemView: View,
+      indicatorPressed: (view: View, position: Int) -> Unit,
+      pressed: (view: View, position: Int) -> Unit
+  ) : RecyclerView.ViewHolder(itemView) {
+    private val artist: TextView by bindView(R.id.line_two)
+    private val title: TextView by bindView(R.id.line_one)
+    private val indicator: ImageView by bindView(R.id.overflow_menu)
+    private val unknownArtist: String by lazy { string(R.string.unknown_artist) }
+
+    init {
+      indicator.setOnClickListener { indicatorPressed(it, adapterPosition) }
+      itemView.setOnClickListener { pressed(it, adapterPosition) }
+    }
+
     companion object {
-      fun create(parent: ViewGroup): ViewHolder {
+      fun create(
+          parent: ViewGroup,
+          indicatorPressed: (view: View, position: Int) -> Unit,
+          pressed: (view: View, position: Int) -> Unit
+      ): ViewHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.ui_list_dual, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, indicatorPressed, pressed)
       }
+    }
+
+    fun clear() {
+      artist.text = ""
+      title.text = ""
+    }
+
+    fun bindTo(trackEntity: TrackEntity) {
+      title.text = trackEntity.title
+      artist.text = if (trackEntity.artist.isBlank()) unknownArtist else trackEntity.artist
     }
   }
 

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/genreartists/GenreArtistsActivity.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.content.library.artists.ArtistEntity
-import com.kelsos.mbrc.content.nowplaying.queue.Queue
+import com.kelsos.mbrc.content.nowplaying.queue.LibraryPopup
 import com.kelsos.mbrc.ui.activities.BaseActivity
 import com.kelsos.mbrc.ui.navigation.library.PopupActionHandler
 import com.kelsos.mbrc.ui.navigation.library.artists.ArtistEntryAdapter
@@ -77,7 +77,7 @@ class GenreArtistsActivity : BaseActivity(),
 
   override fun onMenuItemSelected(@IdRes itemId: Int, artist: ArtistEntity) {
     val action = actionHandler.artistSelected(itemId, artist, this)
-    if (action != Queue.PROFILE) {
+    if (action != LibraryPopup.PROFILE) {
       presenter.queue(action, artist)
     }
   }
@@ -86,8 +86,8 @@ class GenreArtistsActivity : BaseActivity(),
     actionHandler.artistSelected(artist, this)
   }
 
-  override fun update(data: PagedList<ArtistEntity>) {
-    adapter.submitList(data)
+  override fun update(pagedList: PagedList<ArtistEntity>) {
+    adapter.submitList(pagedList)
   }
 
   override fun queue(success: Boolean, tracks: Int) {

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/tracks/TrackEntryAdapter.kt
@@ -6,13 +6,13 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.IdRes
-import androidx.appcompat.widget.PopupMenu
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.content.library.tracks.TrackEntity
 import com.kelsos.mbrc.extensions.string
+import com.kelsos.mbrc.ui.navigation.library.popup
 import com.kelsos.mbrc.utilities.Checks.ifNotNull
 import kotterknife.bindView
 import javax.inject.Inject
@@ -20,47 +20,36 @@ import javax.inject.Inject
 class TrackEntryAdapter
 @Inject
 constructor() : PagedListAdapter<TrackEntity, TrackEntryAdapter.ViewHolder>(DIFF_CALLBACK) {
-
   private var listener: MenuItemSelectedListener? = null
+  private val indicatorPressed: (View, Int) -> Unit = { view, position ->
+    view.popup(R.menu.popup_track) {
+      ifNotNull(listener, getItem(position)) { listener, track ->
+        listener.onMenuItemSelected(it, track)
+      }
+    }
+  }
+
+  private val pressed: (View, Int) -> Unit = { _, position ->
+    ifNotNull(listener, getItem(position)) { listener, track ->
+      listener.onItemClicked(track)
+    }
+  }
 
   fun setMenuItemSelectedListener(listener: MenuItemSelectedListener) {
     this.listener = listener
   }
 
-
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val holder = ViewHolder.create(parent)
-
-    holder.indicator.setOnClickListener { createPopup(it, holder) }
-
-    holder.itemView.setOnClickListener {
-      val position = holder.adapterPosition
-      ifNotNull(listener, getItem(position)) { listener, track ->
-        listener.onItemClicked(track)
-      }
-
-    }
-    return holder
-  }
-
-  private fun createPopup(it: View, holder: ViewHolder) {
-    val popupMenu = PopupMenu(it.context, it)
-    popupMenu.inflate(R.menu.popup_track)
-    popupMenu.setOnMenuItemClickListener { menuItem ->
-      val position = holder.adapterPosition
-      ifNotNull(listener, getItem(position)) { listener, track ->
-        listener.onMenuItemSelected(menuItem.itemId, track)
-      }
-      true
-    }
-    popupMenu.show()
+    return ViewHolder.create(parent, indicatorPressed, pressed)
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    val entry = getItem(holder.adapterPosition)
-    entry?.let { (artist, title) ->
-      holder.title.text = title
-      holder.artist.text = if (artist.isBlank()) holder.unknownArtist else artist
+    val trackEntity = getItem(holder.adapterPosition)
+
+    if (trackEntity != null) {
+      holder.bindTo(trackEntity)
+    } else {
+      holder.clear()
     }
   }
 
@@ -82,18 +71,40 @@ constructor() : PagedListAdapter<TrackEntity, TrackEntryAdapter.ViewHolder>(DIFF
     fun onItemClicked(track: TrackEntity)
   }
 
-  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val artist: TextView by bindView(R.id.line_two)
-    val title: TextView by bindView(R.id.line_one)
-    val indicator: ImageView by bindView(R.id.overflow_menu)
-    val unknownArtist: String by lazy { string(R.string.unknown_artist) }
+  class ViewHolder(
+      itemView: View,
+      indicatorPressed: (view: View, position: Int) -> Unit,
+      pressed: (view: View, position: Int) -> Unit
+  ) : RecyclerView.ViewHolder(itemView) {
+    private val artist: TextView by bindView(R.id.line_two)
+    private val title: TextView by bindView(R.id.line_one)
+    private val indicator: ImageView by bindView(R.id.overflow_menu)
+    private val unknownArtist: String by lazy { string(R.string.unknown_artist) }
 
+    init {
+      indicator.setOnClickListener { indicatorPressed(it, adapterPosition) }
+      itemView.setOnClickListener { pressed(it, adapterPosition) }
+    }
     companion object {
-      fun create(parent: ViewGroup): ViewHolder {
+      fun create(
+          parent: ViewGroup,
+          indicatorPressed: (view: View, position: Int) -> Unit,
+          pressed: (view: View, position: Int) -> Unit
+      ): ViewHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.ui_list_dual, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, indicatorPressed, pressed)
       }
+    }
+
+    fun clear() {
+      artist.text = ""
+      title.text = ""
+    }
+
+    fun bindTo(trackEntity: TrackEntity) {
+      title.text = trackEntity.title
+      artist.text = if (trackEntity.artist.isBlank()) unknownArtist else trackEntity.artist
     }
   }
 