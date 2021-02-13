musicbeeremote/mbrc

Adds a few response models for some of the commands

app/src/main/kotlin/com/kelsos/mbrc/events/UiEvents.kt
@@ -70,11 +70,11 @@ class ShuffleChange(@ShuffleState val shuffleState: String) {
 
 class TrackInfoChangeEvent(val trackInfo: TrackInfo)
 
-class TrackMoved(val from:Int, val to:Int , val success: Boolean)
+class TrackMovedEvent(val from: Int, val to: Int, val success: Boolean)
 
-class TrackRemoval(val index: Int, val success: Boolean)
+class TrackRemovalEvent(val index: Int, val success: Boolean)
 
-class UpdatePosition(val current: Int, val total: Int)
+class UpdatePositionEvent(val current: Int, val total: Int)
 
 class VolumeChange {
   var volume: Int = 0

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/responses/Position.kt
@@ -1,0 +1,9 @@
+package com.kelsos.mbrc.networking.protocol.responses
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Position(
+    @JsonProperty("current") val current: Int,
+    @JsonProperty("total") val total: Int
+)
+

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/main/MainContract.kt
@@ -4,7 +4,7 @@ import com.kelsos.mbrc.content.active_status.PlayerState.State
 import com.kelsos.mbrc.content.active_status.Repeat.Mode
 import com.kelsos.mbrc.content.library.tracks.TrackInfo
 import com.kelsos.mbrc.events.ShuffleChange.ShuffleState
-import com.kelsos.mbrc.events.UpdatePosition
+import com.kelsos.mbrc.events.UpdatePositionEvent
 import com.kelsos.mbrc.mvp.BaseView
 import com.kelsos.mbrc.mvp.Presenter
 import com.kelsos.mbrc.ui.navigation.main.LfmRating.Rating
@@ -29,7 +29,7 @@ interface MainView : BaseView {
 
   fun updateCover(path: String)
 
-  fun updateProgress(position: UpdatePosition)
+  fun updateProgress(position: UpdatePositionEvent)
 
   fun showChangeLog()
 

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/events/UiEvents.kt
@@ -71,11 +71,11 @@ class ShuffleChange(@ShuffleState val shuffleState: String) {
 
 class TrackInfoChangeEvent(val trackInfo: TrackInfo)
 
-class TrackMoved(val from:Int, val to:Int , val success: Boolean)
+class TrackMovedEvent(val from: Int, val to: Int, val success: Boolean)
 
-class TrackRemoval(val index: Int, val success: Boolean)
+class TrackRemovalEvent(val index: Int, val success: Boolean)
 
-class UpdatePosition(val current: Int, val total: Int)
+class UpdatePositionEvent(val current: Long, val total: Long)
 
 class VolumeChange {
   var volume: Int = 0

app/src/main/kotlin/com/kelsos/mbrc/networking/protocol/responses/Position.kt
@@ -1,0 +1,9 @@
+package com.kelsos.mbrc.networking.protocol.responses
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Position(
+    @JsonProperty("current") val current: Long,
+    @JsonProperty("total") val total: Long
+)
+

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/main/MainContract.kt
@@ -4,7 +4,7 @@ import com.kelsos.mbrc.content.active_status.PlayerState.State
 import com.kelsos.mbrc.content.active_status.Repeat.Mode
 import com.kelsos.mbrc.content.library.tracks.TrackInfo
 import com.kelsos.mbrc.events.ShuffleChange.ShuffleState
-import com.kelsos.mbrc.events.UpdatePosition
+import com.kelsos.mbrc.events.UpdatePositionEvent
 import com.kelsos.mbrc.mvp.BaseView
 import com.kelsos.mbrc.mvp.Presenter
 import com.kelsos.mbrc.ui.navigation.main.LfmRating.Rating
@@ -29,7 +29,7 @@ interface MainView : BaseView {
 
   fun updateCover(path: String)
 
-  fun updateProgress(duration: UpdatePosition)
+  fun updateProgress(duration: UpdatePositionEvent)
 
   fun showChangeLog()
 