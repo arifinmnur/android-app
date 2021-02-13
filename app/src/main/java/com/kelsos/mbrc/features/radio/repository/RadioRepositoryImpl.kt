musicbeeremote/mbrc

Fixing the radio feature and adding tests

app/src/main/java/com/kelsos/mbrc/features/radio/repository/RadioRepositoryImpl.kt
@@ -1,7 +1,12 @@
-package com.kelsos.mbrc.content.radios
+package com.kelsos.mbrc.features.radio.repository
 
 import androidx.paging.DataSource
 import arrow.core.Try
+import com.kelsos.mbrc.features.radio.RadioDaoMapper
+import com.kelsos.mbrc.features.radio.RadioDtoMapper
+import com.kelsos.mbrc.features.radio.RadioStationDto
+import com.kelsos.mbrc.features.radio.data.RadioStationDao
+import com.kelsos.mbrc.features.radio.domain.RadioStation
 import com.kelsos.mbrc.networking.ApiBase
 import com.kelsos.mbrc.networking.protocol.Protocol
 import com.kelsos.mbrc.utilities.AppCoroutineDispatchers
@@ -15,15 +20,12 @@ class RadioRepositoryImpl(
   private val dispatchers: AppCoroutineDispatchers
 ) : RadioRepository {
 
-  private val mapper = RadioDtoMapper()
-  private val dao2Model = RadioDaoMapper()
-
   override suspend fun count(): Long {
     return withContext(dispatchers.database) { dao.count() }
   }
 
   override fun getAll(): DataSource.Factory<Int, RadioStation> {
-    return dao.getAll().map { dao2Model.map(it) }
+    return dao.getAll().map { RadioDaoMapper.map(it) }
   }
 
   override suspend fun getRemote(): Try<Unit> = Try {
@@ -31,7 +33,7 @@ class RadioRepositoryImpl(
     val pages = remoteDataSource.getAllPages(Protocol.RadioStations, RadioStationDto::class)
     pages.blockingForEach { page ->
       runBlocking(dispatchers.disk) {
-        val items = page.map { mapper.map(it).apply { dateAdded = added } }
+        val items = page.map { RadioDtoMapper.map(it).apply { dateAdded = added } }
 
         withContext(dispatchers.database) {
           dao.insertAll(items)
@@ -45,7 +47,7 @@ class RadioRepositoryImpl(
   }
 
   override fun search(term: String): DataSource.Factory<Int, RadioStation> {
-    return dao.search(term).map { dao2Model.map(it) }
+    return dao.search(term).map { RadioDaoMapper.map(it) }
   }
 
   override suspend fun cacheIsEmpty(): Boolean {

app/src/test/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModelTest.kt
@@ -1,0 +1,54 @@
+package com.kelsos.mbrc.features.radio.presentation
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import arrow.core.Try
+import com.google.common.truth.Truth.assertThat
+import com.kelsos.mbrc.TestApplication
+import com.kelsos.mbrc.content.nowplaying.queue.QueueApi
+import com.kelsos.mbrc.features.radio.repository.RadioRepository
+import com.kelsos.mbrc.utils.MockFactory
+import com.kelsos.mbrc.utils.TestDispatchers
+import com.kelsos.mbrc.utils.observeOnce
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.net.SocketTimeoutException
+
+@RunWith(AndroidJUnit4::class)
+@Config(application = TestApplication::class)
+class RadioViewModelTest {
+
+  private lateinit var repository: RadioRepository
+  private lateinit var queueApi: QueueApi
+  private lateinit var radioViewModel: RadioViewModel
+
+  @Before
+  fun setUp() {
+    repository = mockk()
+    queueApi = mockk()
+    every { repository.getAll() } answers { MockFactory(emptyList()) }
+    radioViewModel = RadioViewModel(repository, queueApi, TestDispatchers.dispatchers)
+  }
+
+  @Test
+  fun `should notify the observer that refresh failed`() {
+    coEvery { repository.getRemote() } coAnswers { Try.raiseError(SocketTimeoutException()) }
+    radioViewModel.reload()
+    radioViewModel.emitter.observeOnce { event ->
+      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
+    }
+  }
+
+  @Test
+  fun `should notify the observer that refresh succeeded`() {
+    coEvery { repository.getRemote() } coAnswers { Try.invoke { } }
+    radioViewModel.reload()
+    radioViewModel.emitter.observeOnce { event ->
+      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
+    }
+  }
+}

app/src/test/java/com/kelsos/mbrc/features/radio/repository/RadioRepositoryTest.kt
@@ -1,0 +1,123 @@
+package com.kelsos.mbrc.features.radio.repository
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.kelsos.mbrc.TestApplication
+import com.kelsos.mbrc.data.Database
+import com.kelsos.mbrc.features.radio.RadioStationDto
+import com.kelsos.mbrc.features.radio.data.RadioStationDao
+import com.kelsos.mbrc.features.radio.domain.RadioStation
+import com.kelsos.mbrc.networking.ApiBase
+import com.kelsos.mbrc.networking.protocol.Protocol
+import com.kelsos.mbrc.utilities.paged
+import com.kelsos.mbrc.utils.TestData
+import com.kelsos.mbrc.utils.TestData.mockApi
+import com.kelsos.mbrc.utils.observeOnce
+import com.kelsos.mbrc.utils.testDispatcherModule
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.dsl.module.module
+import org.koin.experimental.builder.create
+import org.koin.standalone.StandAloneContext
+import org.koin.standalone.StandAloneContext.startKoin
+import org.koin.standalone.inject
+import org.koin.test.KoinTest
+import org.robolectric.annotation.Config
+import java.net.SocketTimeoutException
+
+@RunWith(AndroidJUnit4::class)
+@Config(application = TestApplication::class)
+class RadioRepositoryTest : KoinTest {
+
+  private val repository: RadioRepository by inject()
+
+  private lateinit var db: Database
+  private lateinit var dao: RadioStationDao
+  private lateinit var apiBase: ApiBase
+
+  @get:Rule
+  val rule = InstantTaskExecutorRule()
+
+  @Before
+  fun setUp() {
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    db = TestData.createDB(context)
+    dao = db.radioStationDao()
+    apiBase = mockk()
+
+    startKoin(listOf(module {
+      single { dao }
+      single<RadioRepository> { create<RadioRepositoryImpl>() }
+      single { apiBase }
+    }, testDispatcherModule))
+  }
+
+  @After
+  fun tearDown() {
+    db.close()
+    StandAloneContext.stopKoin()
+  }
+
+  @Test
+  fun `sync is failure if there is an exception`() {
+    every {
+      apiBase.getAllPages(
+        Protocol.RadioStations,
+        RadioStationDto::class
+      )
+    } throws SocketTimeoutException()
+    runBlocking {
+      assertThat(repository.getRemote().isFailure()).isTrue()
+    }
+  }
+
+  @Test
+  fun `sync remote data and update the database`() {
+    runBlocking { assertThat(repository.cacheIsEmpty()) }
+    every { apiBase.getAllPages(Protocol.RadioStations, RadioStationDto::class) } answers {
+      mockApi(2) {
+        RadioStationDto(name = "Radio $it", url = "http://radio.statio/$it")
+      }
+    }
+
+    runBlocking {
+      assertThat(repository.getRemote().isSuccess()).isTrue()
+      assertThat(repository.count()).isEqualTo(2)
+      repository.getAll().paged().observeOnce { result ->
+        assertThat(result).hasSize(2)
+      }
+    }
+  }
+
+  @Test
+  fun `it should filter the stations when searching`() {
+    every { apiBase.getAllPages(Protocol.RadioStations, RadioStationDto::class) } answers {
+      mockApi(5, listOf(RadioStationDto(name = "Heavy Metal", url = "http://heavy.metal.ru"))) {
+        RadioStationDto(name = "Radio $it", url = "http://radio.statio/$it")
+      }
+    }
+
+    runBlocking {
+      assertThat(repository.getRemote().isSuccess()).isTrue()
+      repository.search("Metal").paged().observeOnce {
+        assertThat(it).hasSize(1)
+        assertThat(it).containsExactly(
+          RadioStation(
+            name = "Heavy Metal",
+            url = "http://heavy.metal.ru",
+            id = 6
+          )
+        )
+      }
+    }
+  }
+}

arifinmnur/android-app

app/src/main/java/com/kelsos/mbrc/features/radio/repository/RadioRepositoryImpl.kt
@@ -1,8 +1,13 @@
-package com.kelsos.mbrc.content.radios
+package com.kelsos.mbrc.features.radio.repository
 
 import androidx.paging.DataSource
 import arrow.core.Try
 import com.kelsos.mbrc.di.modules.AppCoroutineDispatchers
+import com.kelsos.mbrc.features.radio.RadioDaoMapper
+import com.kelsos.mbrc.features.radio.RadioDtoMapper
+import com.kelsos.mbrc.features.radio.RadioStationDto
+import com.kelsos.mbrc.features.radio.data.RadioStationDao
+import com.kelsos.mbrc.features.radio.domain.RadioStation
 import com.kelsos.mbrc.networking.ApiBase
 import com.kelsos.mbrc.networking.protocol.Protocol
 import com.kelsos.mbrc.utilities.epoch
@@ -15,13 +20,13 @@ class RadioRepositoryImpl(
   private val api: ApiBase,
   private val dispatchers: AppCoroutineDispatchers
 ) : RadioRepository {
-  private val mapper = RadioDtoMapper()
-  private val dao2Model = RadioDaoMapper()
 
-  override suspend fun count(): Long = withContext(dispatchers.database) { dao.count() }
+  override suspend fun count(): Long = withContext(dispatchers.database) {
+    dao.count()
+  }
 
   override fun getAll(): DataSource.Factory<Int, RadioStation> {
-    return dao.getAll().map { dao2Model.map(it) }
+    return dao.getAll().map { RadioDaoMapper.map(it) }
   }
 
   override suspend fun getRemote(): Try<Unit> = Try {
@@ -33,7 +38,7 @@ class RadioRepositoryImpl(
             dao.removePreviousEntries(added)
           }
         }.collect {
-          val items = it.map { mapper.map(it).apply { dateAdded = added } }
+          val items = it.map { RadioDtoMapper.map(it).apply { dateAdded = added } }
           withContext(dispatchers.database) {
             withContext(dispatchers.database) {
               dao.insertAll(items)
@@ -44,7 +49,7 @@ class RadioRepositoryImpl(
   }
 
   override fun search(term: String): DataSource.Factory<Int, RadioStation> {
-    return dao.search(term).map { dao2Model.map(it) }
+    return dao.search(term).map { RadioDaoMapper.map(it) }
   }
 
   override suspend fun cacheIsEmpty(): Boolean =

app/src/test/java/com/kelsos/mbrc/features/radio/presentation/RadioViewModelTest.kt
@@ -1,0 +1,54 @@
+package com.kelsos.mbrc.features.radio.presentation
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import arrow.core.Try
+import com.google.common.truth.Truth.assertThat
+import com.kelsos.mbrc.TestApplication
+import com.kelsos.mbrc.features.radio.repository.RadioRepository
+import com.kelsos.mbrc.helper.QueueHandler
+import com.kelsos.mbrc.utils.MockFactory
+import com.kelsos.mbrc.utils.TestDispatchers
+import com.kelsos.mbrc.utils.observeOnce
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.net.SocketTimeoutException
+
+@RunWith(AndroidJUnit4::class)
+@Config(application = TestApplication::class)
+class RadioViewModelTest {
+
+  private lateinit var repository: RadioRepository
+  private lateinit var queueApi: QueueHandler
+  private lateinit var radioViewModel: RadioViewModel
+
+  @Before
+  fun setUp() {
+    repository = mockk()
+    queueApi = mockk()
+    every { repository.getAll() } answers { MockFactory(emptyList()) }
+    radioViewModel = RadioViewModel(repository, queueApi, TestDispatchers.dispatchers)
+  }
+
+  @Test
+  fun `should notify the observer that refresh failed`() {
+    coEvery { repository.getRemote() } coAnswers { Try.raiseError(SocketTimeoutException()) }
+    radioViewModel.reload()
+    radioViewModel.emitter.observeOnce { event ->
+      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
+    }
+  }
+
+  @Test
+  fun `should notify the observer that refresh succeeded`() {
+    coEvery { repository.getRemote() } coAnswers { Try.invoke { } }
+    radioViewModel.reload()
+    radioViewModel.emitter.observeOnce { event ->
+      assertThat(event.peekContent()).isEqualTo(RadioRefreshResult.RefreshFailed)
+    }
+  }
+}

app/src/test/java/com/kelsos/mbrc/features/radio/repository/RadioRepositoryTest.kt
@@ -1,0 +1,124 @@
+package com.kelsos.mbrc.features.radio.repository
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.kelsos.mbrc.TestApplication
+import com.kelsos.mbrc.data.Database
+import com.kelsos.mbrc.features.radio.RadioStationDto
+import com.kelsos.mbrc.features.radio.data.RadioStationDao
+import com.kelsos.mbrc.features.radio.domain.RadioStation
+import com.kelsos.mbrc.networking.ApiBase
+import com.kelsos.mbrc.networking.protocol.Protocol
+import com.kelsos.mbrc.utilities.paged
+import com.kelsos.mbrc.utils.TestData
+import com.kelsos.mbrc.utils.TestData.mockApi
+import com.kelsos.mbrc.utils.observeOnce
+import com.kelsos.mbrc.utils.testDispatcherModule
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.rx2.asFlow
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.dsl.module.module
+import org.koin.experimental.builder.create
+import org.koin.standalone.StandAloneContext
+import org.koin.standalone.StandAloneContext.startKoin
+import org.koin.standalone.inject
+import org.koin.test.KoinTest
+import org.robolectric.annotation.Config
+import java.net.SocketTimeoutException
+
+@RunWith(AndroidJUnit4::class)
+@Config(application = TestApplication::class)
+class RadioRepositoryTest : KoinTest {
+
+  private val repository: RadioRepository by inject()
+
+  private lateinit var db: Database
+  private lateinit var dao: RadioStationDao
+  private lateinit var apiBase: ApiBase
+
+  @get:Rule
+  val rule = InstantTaskExecutorRule()
+
+  @Before
+  fun setUp() {
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    db = TestData.createDB(context)
+    dao = db.radioStationDao()
+    apiBase = mockk()
+
+    startKoin(listOf(module {
+      single { dao }
+      single<RadioRepository> { create<RadioRepositoryImpl>() }
+      single { apiBase }
+    }, testDispatcherModule))
+  }
+
+  @After
+  fun tearDown() {
+    db.close()
+    StandAloneContext.stopKoin()
+  }
+
+  @Test
+  fun `sync is failure if there is an exception`() {
+    coEvery {
+      apiBase.getAllPages(
+        Protocol.RadioStations,
+        RadioStationDto::class
+      )
+    } throws SocketTimeoutException()
+    runBlocking {
+      assertThat(repository.getRemote().isFailure()).isTrue()
+    }
+  }
+
+  @Test
+  fun `sync remote data and update the database`() {
+    runBlocking { assertThat(repository.cacheIsEmpty()) }
+    coEvery { apiBase.getAllPages(Protocol.RadioStations, RadioStationDto::class) } answers {
+      mockApi(2) {
+        RadioStationDto(name = "Radio $it", url = "http://radio.statio/$it")
+      }.asFlow()
+    }
+
+    runBlocking {
+      assertThat(repository.getRemote().isSuccess()).isTrue()
+      assertThat(repository.count()).isEqualTo(2)
+      repository.getAll().paged().observeOnce { result ->
+        assertThat(result).hasSize(2)
+      }
+    }
+  }
+
+  @Test
+  fun `it should filter the stations when searching`() {
+    coEvery { apiBase.getAllPages(Protocol.RadioStations, RadioStationDto::class) } answers {
+      mockApi(5, listOf(RadioStationDto(name = "Heavy Metal", url = "http://heavy.metal.ru"))) {
+        RadioStationDto(name = "Radio $it", url = "http://radio.statio/$it")
+      }.asFlow()
+    }
+
+    runBlocking {
+      assertThat(repository.getRemote().isSuccess()).isTrue()
+      repository.search("Metal").paged().observeOnce {
+        assertThat(it).hasSize(1)
+        assertThat(it).containsExactly(
+          RadioStation(
+            name = "Heavy Metal",
+            url = "http://heavy.metal.ru",
+            id = 6
+          )
+        )
+      }
+    }
+  }
+}