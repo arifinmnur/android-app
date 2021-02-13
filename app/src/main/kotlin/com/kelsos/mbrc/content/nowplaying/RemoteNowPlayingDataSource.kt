musicbeeremote/mbrc

Migrating from DbFlow to room

app/src/main/kotlin/com/kelsos/mbrc/content/nowplaying/RemoteNowPlayingDataSource.kt
@@ -5,8 +5,9 @@ import io.reactivex.Observable
 import javax.inject.Inject
 
 class RemoteNowPlayingDataSource
-@Inject constructor(private val service: NowPlayingService) : RemoteDataSource<NowPlaying> {
-  override fun fetch(): Observable<List<NowPlaying>> {
+@Inject
+constructor(private val service: NowPlayingService) : RemoteDataSource<NowPlayingDto> {
+  override fun fetch(): Observable<List<NowPlayingDto>> {
     return Observable.range(0, Integer.MAX_VALUE).flatMap {
       service.getNowPlaying(it * RemoteDataSource.LIMIT, RemoteDataSource.LIMIT)
     }.takeWhile { it.offset < it.total }.map { it.data }

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/albums/BrowseAlbumFragment.kt
@@ -14,7 +14,7 @@ import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.kelsos.mbrc.R
-import com.kelsos.mbrc.content.library.albums.Album
+import com.kelsos.mbrc.content.library.albums.AlbumEntity
 import com.kelsos.mbrc.extensions.fail
 import com.kelsos.mbrc.extensions.initLinear
 import com.kelsos.mbrc.ui.dialogs.SortingDialog
@@ -95,12 +95,12 @@ class BrowseAlbumFragment : Fragment(),
     presenter.load()
   }
 
-  override fun onMenuItemSelected(menuItem: MenuItem, entry: Album) {
+  override fun onMenuItemSelected(menuItem: MenuItem, entry: AlbumEntity) {
     val activity = activity ?: fail("null activity")
     actionHandler.albumSelected(menuItem, entry, activity)
   }
 
-  override fun onItemClicked(album: Album) {
+  override fun onItemClicked(album: AlbumEntity) {
     val activity = activity ?: fail("null activity")
     actionHandler.albumSelected(album, activity)
   }
@@ -113,7 +113,7 @@ class BrowseAlbumFragment : Fragment(),
     presenter.reload()
   }
 
-  override fun update(cursor: List<Album>) {
+  override fun update(cursor: List<AlbumEntity>) {
     adapter.update(cursor)
     swipeLayout.isRefreshing = false
   }

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/content/nowplaying/RemoteNowPlayingDataSource.kt
@@ -7,8 +7,9 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RemoteNowPlayingDataSource
-@Inject constructor(private val service: ApiBase) : RemoteDataSource<NowPlaying> {
-  override suspend fun fetch(): Flow<List<NowPlaying>> {
-    return service.getAllPages(Protocol.NowPlayingList, NowPlaying::class)
+@Inject
+constructor(private val service: ApiBase) : RemoteDataSource<NowPlayingDto> {
+  override suspend fun fetch(): Flow<List<NowPlayingDto>> {
+    return service.getAllPages(Protocol.NowPlayingList, NowPlayingDto::class)
   }
 }

app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/albums/BrowseAlbumFragment.kt
@@ -16,7 +16,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.kelsos.mbrc.R
-import com.kelsos.mbrc.content.library.albums.Album
+import com.kelsos.mbrc.content.library.albums.AlbumEntity
 import com.kelsos.mbrc.content.nowplaying.queue.Queue
 import com.kelsos.mbrc.extensions.fail
 import com.kelsos.mbrc.ui.dialogs.SortingDialog
@@ -49,7 +49,7 @@ class BrowseAlbumFragment : Fragment(),
   @Inject
   lateinit var presenter: BrowseAlbumPresenter
 
-  private lateinit var syncButton: Button;
+  private lateinit var syncButton: Button
 
   override fun search(term: String) {
     syncButton.isGone = term.isNotEmpty()
@@ -98,7 +98,7 @@ class BrowseAlbumFragment : Fragment(),
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     emptyViewTitle.setText(R.string.albums_list_empty)
-    syncButton = view.findViewById(R.id.list_empty_sync);
+    syncButton = view.findViewById(R.id.list_empty_sync)
     syncButton.setOnClickListener {
       presenter.sync()
     }
@@ -111,18 +111,18 @@ class BrowseAlbumFragment : Fragment(),
     presenter.load()
   }
 
-  override fun onMenuItemSelected(menuItem: MenuItem, album: Album) {
+  override fun onMenuItemSelected(menuItem: MenuItem, album: AlbumEntity) {
     val action = actionHandler.albumSelected(menuItem, album, requireActivity())
     if (action != Queue.PROFILE) {
       presenter.queue(action, album)
     }
   }
 
-  override fun onItemClicked(album: Album) {
+  override fun onItemClicked(album: AlbumEntity) {
     actionHandler.albumSelected(album, requireActivity())
   }
 
-  override fun update(cursor: List<Album>) {
+  override fun update(cursor: List<AlbumEntity>) {
     adapter.update(cursor)
   }
 