musicbeeremote/mbrc

Restructuring annotation classes to proper packages

app/src/main/kotlin/com/kelsos/mbrc/content/now_playing/queue/QueuePayload.kt
@@ -1,7 +1,7 @@
 package com.kelsos.mbrc.content.now_playing.queue
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.kelsos.mbrc.annotations.Queue.QueueType
+import com.kelsos.mbrc.content.now_playing.queue.Queue.QueueType
 
 data class QueuePayload(@JsonProperty("queue") @QueueType val type: String,
                         @JsonProperty("data") val data: List<String>,

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/content/now_playing/queue/QueuePayload.kt
@@ -1,7 +1,7 @@
 package com.kelsos.mbrc.content.now_playing.queue
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.kelsos.mbrc.annotations.Queue.Action
+import com.kelsos.mbrc.content.now_playing.queue.Queue.Action
 
 data class QueuePayload(
   @JsonProperty("queue") @Action val type: String,