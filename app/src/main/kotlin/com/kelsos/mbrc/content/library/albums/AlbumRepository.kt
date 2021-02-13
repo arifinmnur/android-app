musicbeeremote/mbrc

WIP: Album sorting functionality

app/src/main/kotlin/com/kelsos/mbrc/content/library/albums/AlbumRepository.kt
@@ -11,7 +11,7 @@ interface AlbumRepository : Repository<Album> {
    * Retrieves the albums ordered by
    */
   fun getAlbumsSorted(
-      @Sorting.Order order: Long = Sorting.ALBUM_ARTIST__ALBUM,
+      @Sorting.Fields order: Long = Sorting.ALBUM_ARTIST__ALBUM,
       ascending: Boolean = true
   ): Single<FlowCursorList<Album>>
 }

app/src/main/kotlin/com/kelsos/mbrc/preferences/AlbumSortingStore.kt
@@ -1,0 +1,16 @@
+package com.kelsos.mbrc.preferences
+
+import com.kelsos.mbrc.content.library.albums.Sorting.Fields
+import com.kelsos.mbrc.content.library.albums.Sorting.Order
+
+interface AlbumSortingStore {
+  @Fields
+  fun getSortingSelection(): Long
+
+  fun setSortingSelection(@Fields sorting: Long)
+
+  @Order
+  fun getSortingOrder(): Long
+
+  fun setSortingOrder(@Order order: Long)
+}

app/src/main/kotlin/com/kelsos/mbrc/preferences/AlbumSortingStoreImpl.kt
@@ -1,0 +1,36 @@
+package com.kelsos.mbrc.preferences
+
+import android.content.SharedPreferences
+import com.kelsos.mbrc.content.library.albums.Sorting
+import javax.inject.Inject
+
+class AlbumSortingStoreImpl
+@Inject
+constructor(private val sharedPreferences: SharedPreferences) : AlbumSortingStore {
+  @Sorting.Fields
+  override fun getSortingSelection(): Long {
+    return sharedPreferences.getLong(ALBUM_SORTING_KEY, Sorting.ALBUM_ARTIST__ALBUM)
+  }
+
+  override fun setSortingSelection(@Sorting.Fields sorting: Long) {
+    sharedPreferences.edit()
+        .putLong(ALBUM_SORTING_KEY, sorting)
+        .apply()
+  }
+
+  @Sorting.Order
+  override fun getSortingOrder(): Long {
+    return sharedPreferences.getLong(ALBUM_SORTING_ORDER_KEY, Sorting.ORDER_ASCENDING)
+  }
+
+  override fun setSortingOrder(@Sorting.Order order: Long) {
+    sharedPreferences.edit()
+        .putLong(ALBUM_SORTING_ORDER_KEY, order)
+        .apply()
+  }
+
+  companion object {
+    private const val ALBUM_SORTING_KEY = "com.kelsos.mbrc.preferences.ALBUM_SORTING"
+    private const val ALBUM_SORTING_ORDER_KEY = "com.kelsos.mbrc.preferences.ALBUM_SORTING_ORDER"
+  }
+}

arifinmnur/android-app

app/src/main/kotlin/com/kelsos/mbrc/content/library/albums/AlbumRepository.kt
@@ -10,7 +10,7 @@ interface AlbumRepository : Repository<Album> {
    * Retrieves the albums ordered by
    */
   suspend fun getAlbumsSorted(
-      @Sorting.Order order: Long = Sorting.ALBUM_ARTIST__ALBUM,
+      @Sorting.Fields order: Int = Sorting.ALBUM_ARTIST__ALBUM,
       ascending: Boolean = true
   ): FlowCursorList<Album>
 }

app/src/main/kotlin/com/kelsos/mbrc/preferences/AlbumSortingStore.kt
@@ -1,0 +1,16 @@
+package com.kelsos.mbrc.preferences
+
+import com.kelsos.mbrc.content.library.albums.Sorting.Fields
+import com.kelsos.mbrc.content.library.albums.Sorting.Order
+
+interface AlbumSortingStore {
+  @Fields
+  fun getSortingSelection(): Int
+
+  fun setSortingSelection(@Fields sorting: Int)
+
+  @Order
+  fun getSortingOrder(): Int
+
+  fun setSortingOrder(@Order order: Int)
+}

app/src/main/kotlin/com/kelsos/mbrc/preferences/AlbumSortingStoreImpl.kt
@@ -1,0 +1,36 @@
+package com.kelsos.mbrc.preferences
+
+import android.content.SharedPreferences
+import com.kelsos.mbrc.content.library.albums.Sorting
+import javax.inject.Inject
+
+class AlbumSortingStoreImpl
+@Inject
+constructor(private val sharedPreferences: SharedPreferences) : AlbumSortingStore {
+  @Sorting.Fields
+  override fun getSortingSelection(): Int {
+    return sharedPreferences.getInt(ALBUM_SORTING_KEY, Sorting.ALBUM_ARTIST__ALBUM)
+  }
+
+  override fun setSortingSelection(@Sorting.Fields sorting: Int) {
+    sharedPreferences.edit()
+        .putInt(ALBUM_SORTING_KEY, sorting)
+        .apply()
+  }
+
+  @Sorting.Order
+  override fun getSortingOrder(): Int {
+    return sharedPreferences.getInt(ALBUM_SORTING_ORDER_KEY, Sorting.ORDER_ASCENDING)
+  }
+
+  override fun setSortingOrder(@Sorting.Order order: Int) {
+    sharedPreferences.edit()
+        .putInt(ALBUM_SORTING_ORDER_KEY, order)
+        .apply()
+  }
+
+  companion object {
+    private const val ALBUM_SORTING_KEY = "com.kelsos.mbrc.preferences.ALBUM_SORTING"
+    private const val ALBUM_SORTING_ORDER_KEY = "com.kelsos.mbrc.preferences.ALBUM_SORTING_ORDER"
+  }
+}